ad3d77fa94b81da0ed0c93472fcda5408d1309ef
guangzhou.chen <guangzhou.chen@qunar.com>
2016-04-10 23:30:21 +0800

    fix config

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+var search = hexo.theme.search || {};
+
+search.path = search.path || 'search.json';
+
+hexo.theme.search = search;
+
 hexo.extend.generator.register('images', require('./lib/generator'));
